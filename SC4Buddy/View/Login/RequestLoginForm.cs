345cba556300eca3be3d712eb279787101bac177
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-14 20:51:32 +0200

    formatted usings and added messagebox methods for help fields

--- SC4Buddy/View/Login/RequestLoginForm.cs
+++ SC4Buddy/View/Login/RequestLoginForm.cs
@@ -1,20 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace NIHEI.SC4Buddy.View.Login
+﻿namespace NIHEI.SC4Buddy.View.Login
 {
+    using System;
+    using System.Windows.Forms;
+
     public partial class RequestLoginForm : Form
     {
         public RequestLoginForm()
         {
             InitializeComponent();
+        }
+
+        private void SitePictureBoxClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                this,
+                "The URL to the website where you have uploaded your content."
+                + " (Enter only 1 URL, you can add more sites when your user has been created)",
+                "Site URL help",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1);
+        }
+
+        private void UsernamePictureBoxClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                this,
+                "Your username (what others see and search after) on the site you entered above.",
+                "Username help",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1);
         }
     }
 }
