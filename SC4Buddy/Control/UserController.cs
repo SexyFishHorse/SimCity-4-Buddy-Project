d2552118669ff0c2cbe5849bccd22733a1ae061b
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-14 23:08:22 +0200

    moved private methods to top of class in user controller

--- SC4Buddy/Control/UserController.cs
+++ SC4Buddy/Control/UserController.cs
@@ -24,36 +24,6 @@
             this.authorRegistry = authorRegistry;
         }
 
-        private static bool CompareByteArrays(byte[] array1, byte[] array2)
-        {
-            if (array1.Length != array2.Length)
-            {
-                return false;
-            }
-
-            return !array1.Where((t, i) => t != array2[i]).Any();
-        }
-
-        private static byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
-        {
-            HashAlgorithm algorithm = new SHA256Managed();
-
-            var plainTextWithSaltBytes =
-              new byte[plainText.Length + salt.Length];
-
-            for (var i = 0; i < plainText.Length; i++)
-            {
-                plainTextWithSaltBytes[i] = plainText[i];
-            }
-
-            for (var i = 0; i < salt.Length; i++)
-            {
-                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
-            }
-
-            return algorithm.ComputeHash(plainTextWithSaltBytes);
-        }
-
         public User Login(string email, string password)
         {
             var possibleUser =
@@ -112,6 +82,36 @@
             user.Authors.Add(author);
 
             userRegistry.Update(user);
+        }
+
+        private static bool CompareByteArrays(byte[] array1, byte[] array2)
+        {
+            if (array1.Length != array2.Length)
+            {
+                return false;
+            }
+
+            return !array1.Where((t, i) => t != array2[i]).Any();
+        }
+
+        private static byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
+        {
+            HashAlgorithm algorithm = new SHA256Managed();
+
+            var plainTextWithSaltBytes =
+              new byte[plainText.Length + salt.Length];
+
+            for (var i = 0; i < plainText.Length; i++)
+            {
+                plainTextWithSaltBytes[i] = plainText[i];
+            }
+
+            for (var i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltBytes);
         }
 
         private bool EmailSeemsValid(string email)
