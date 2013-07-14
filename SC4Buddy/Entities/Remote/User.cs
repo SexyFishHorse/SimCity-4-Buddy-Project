88d93905c0830a9a5a40835dd662720a1033ce30
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-14 04:40:18 +0200

    added partial user class to help with identifying user rights

---
+++ SC4Buddy/Entities/Remote/User.cs
@@ -0,0 +1,21 @@
+﻿namespace NIHEI.SC4Buddy.Entities.Remote
+{
+    public partial class User
+    {
+        public bool IsDeveloper
+        {
+            get
+            {
+                return Rights.Equals("Developer");
+            }
+        }
+
+        public bool IsAuthor
+        {
+            get
+            {
+                return Rights.Equals("Author");
+            }
+        }
+    }
+}
