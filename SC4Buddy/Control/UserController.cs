a1f0518a996f9e33b0ff8ee26df21aed85dad165
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-14 23:04:31 +0200

    added generatesaltedhash method

--- SC4Buddy/Control/UserController.cs
+++ SC4Buddy/Control/UserController.cs
@@ -138,5 +138,25 @@
 
             return true;
         }
+
+        private static byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
+        {
+            HashAlgorithm algorithm = new SHA256Managed();
+
+            var plainTextWithSaltBytes =
+              new byte[plainText.Length + salt.Length];
+
+            for (var i = 0; i < plainText.Length; i++)
+            {
+                plainTextWithSaltBytes[i] = plainText[i];
+            }
+
+            for (var i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltBytes);
+        }
     }
 }
