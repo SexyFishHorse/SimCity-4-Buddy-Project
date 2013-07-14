93eabeea706274e14de9036cdf8026953ff2e927
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-14 15:24:17 +0200

    default user folder (in documents) is now detected and set during first settings setup

--- SC4Buddy/Program.cs
+++ SC4Buddy/Program.cs
@@ -3,9 +3,14 @@
     using System;
     using System.Diagnostics;
     using System.Globalization;
+    using System.IO;
+    using System.Linq;
     using System.Threading;
     using System.Windows.Forms;
 
+    using NIHEI.SC4Buddy.DataAccess;
+    using NIHEI.SC4Buddy.Entities;
+    using NIHEI.SC4Buddy.Localization;
     using NIHEI.SC4Buddy.Properties;
     using NIHEI.SC4Buddy.View.Application;
 
@@ -24,11 +29,25 @@
             if (string.IsNullOrWhiteSpace(Settings.Default.GameLocation))
             {
                 Application.Run(new SettingsForm { StartPosition = FormStartPosition.CenterScreen });
+                SetDefaultUserFolder();
             }
 
             if (!string.IsNullOrWhiteSpace(Settings.Default.GameLocation))
             {
                 Application.Run(new Sc4Buddy());
+            }
+        }
+
+        private static void SetDefaultUserFolder()
+        {
+            var registry = RegistryFactory.UserFolderRegistry;
+
+            var path = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SimCity 4");
+
+            if (Directory.Exists(path) && !registry.UserFolders.Any(x => x.Path.Equals(path)))
+            {
+                registry.Add(new UserFolder { Alias = LocalizationStrings.DefaultUserFolderName, Path = path });
             }
         }
     }
