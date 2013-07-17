beb79555ff326fba2ea777d80ce079051b741834
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-17 19:50:12 +0200

    added populate listview to load method

--- SC4Buddy/View/Author/MyAuthorsForm.cs
+++ SC4Buddy/View/Author/MyAuthorsForm.cs
@@ -1,9 +1,11 @@
 ﻿namespace NIHEI.SC4Buddy.View.Author
 {
     using System;
+    using System.Linq;
     using System.Windows.Forms;
 
     using NIHEI.SC4Buddy.Control;
+    using NIHEI.SC4Buddy.DataAccess.Remote;
     using NIHEI.SC4Buddy.View.Elements;
 
     public partial class MyAuthorsForm : Form
@@ -29,6 +31,20 @@
 
         private void MyAuthorsFormLoad(object sender, EventArgs e)
         {
+            var authors = registry.Authors.Where(x => x.UserId == SessionController.Instance.User.Id);
+
+            authorsListView.BeginUpdate();
+            authorsListView.Items.Clear();
+            foreach (var author in authors)
+            {
+                var item = new ListViewItemWithObjectValue<Entities.Remote.Author>(author.Name, author);
+                item.SubItems.Add(author.Site);
+                authorsListView.Items.Add(item);
+            }
+
+            authorsListView.EndUpdate();
+
+            authorsListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
         }
     }
 }
