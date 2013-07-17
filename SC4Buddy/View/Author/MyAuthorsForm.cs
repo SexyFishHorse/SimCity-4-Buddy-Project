682972c1e131fd26a4ac899b588912a592b51a7e
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-17 16:48:35 +0200

    added my authors form

---
+++ SC4Buddy/View/Author/MyAuthorsForm.cs
@@ -0,0 +1,13 @@
+﻿namespace NIHEI.SC4Buddy.View.Author
+{
+    using System;
+    using System.Windows.Forms;
+
+    public partial class MyAuthorsForm : Form
+    {
+        public MyAuthorsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
