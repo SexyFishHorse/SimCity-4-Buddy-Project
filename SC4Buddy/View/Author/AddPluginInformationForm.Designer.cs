79e0cd3a2bb2f70c8b3a57b625417c45629228f0
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-18 08:09:40 +0200

    added fields and buttons to add plugin information form

--- SC4Buddy/View/Author/AddPluginInformationForm.Designer.cs
+++ SC4Buddy/View/Author/AddPluginInformationForm.Designer.cs
@@ -28,19 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.siteAndAuthorComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.filesButton = new System.Windows.Forms.Button();
+            this.dependenciesButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nameTextBox.Location = new System.Drawing.Point(155, 12);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(408, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Site and author";
+            // 
+            // siteAndAuthorComboBox
+            // 
+            this.siteAndAuthorComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.siteAndAuthorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.siteAndAuthorComboBox.FormattingEnabled = true;
+            this.siteAndAuthorComboBox.Location = new System.Drawing.Point(155, 38);
+            this.siteAndAuthorComboBox.Name = "siteAndAuthorComboBox";
+            this.siteAndAuthorComboBox.Size = new System.Drawing.Size(408, 21);
+            this.siteAndAuthorComboBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Link to info/download page";
+            // 
+            // linkTextBox
+            // 
+            this.linkTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkTextBox.Location = new System.Drawing.Point(155, 65);
+            this.linkTextBox.Name = "linkTextBox";
+            this.linkTextBox.Size = new System.Drawing.Size(408, 20);
+            this.linkTextBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Description";
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.descriptionTextBox.Location = new System.Drawing.Point(12, 110);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(551, 200);
+            this.descriptionTextBox.TabIndex = 7;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(488, 316);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 8;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(407, 316);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 9;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // filesButton
+            // 
+            this.filesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.filesButton.Location = new System.Drawing.Point(12, 316);
+            this.filesButton.Name = "filesButton";
+            this.filesButton.Size = new System.Drawing.Size(75, 23);
+            this.filesButton.TabIndex = 10;
+            this.filesButton.Text = "Files";
+            this.filesButton.UseVisualStyleBackColor = true;
+            // 
+            // dependenciesButton
+            // 
+            this.dependenciesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dependenciesButton.Location = new System.Drawing.Point(93, 316);
+            this.dependenciesButton.Name = "dependenciesButton";
+            this.dependenciesButton.Size = new System.Drawing.Size(90, 23);
+            this.dependenciesButton.TabIndex = 11;
+            this.dependenciesButton.Text = "Dependencies";
+            this.dependenciesButton.UseVisualStyleBackColor = true;
             // 
             // AddPluginInformationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(661, 389);
+            this.ClientSize = new System.Drawing.Size(575, 351);
+            this.Controls.Add(this.dependenciesButton);
+            this.Controls.Add(this.filesButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.siteAndAuthorComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label1);
             this.Name = "AddPluginInformationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Add plugin information to the central database";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox siteAndAuthorComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox linkTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button filesButton;
+        private System.Windows.Forms.Button dependenciesButton;
     }
 }
