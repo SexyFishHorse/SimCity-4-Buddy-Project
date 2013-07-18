e6d2505dc09fcc12c01e743341bf12e65f85425c
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-18 07:59:52 +0200

    resized and changed the title of the addplugininformationform

--- SC4Buddy/View/Author/AddPluginInformationForm.Designer.cs
+++ SC4Buddy/View/Author/AddPluginInformationForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AddPluginInformationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "AddPluginInformationForm";
+            this.ClientSize = new System.Drawing.Size(661, 389);
+            this.Name = "AddPluginInformationForm";
+            this.Text = "Add plugin information to the central database";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
