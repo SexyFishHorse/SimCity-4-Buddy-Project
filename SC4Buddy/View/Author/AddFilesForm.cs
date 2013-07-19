7075fb28450a37b3af9783356cd885318cac9f0e
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-19 13:41:27 +0200

    added files list to addfilesform

--- SC4Buddy/View/Author/AddFilesForm.cs
+++ SC4Buddy/View/Author/AddFilesForm.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace NIHEI.SC4Buddy.View.Author
+﻿namespace NIHEI.SC4Buddy.View.Author
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Windows.Forms;
+
+    using NIHEI.Common.IO;
+    using NIHEI.Common.UI.Elements;
+    using NIHEI.SC4Buddy.Entities.Remote;
+
     public partial class AddFilesForm : Form
     {
+        private readonly IList<RemotePluginFile> files;
+
         public AddFilesForm()
         {
             InitializeComponent();
+
+            files = new List<RemotePluginFile>();
+        }
         }
     }
 }
