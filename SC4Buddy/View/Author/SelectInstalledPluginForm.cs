053c3c4e29b985a658a1f30f48e6009d5c01209e
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-20 19:34:41 +0200

    user folder combobox is now populated with items

--- SC4Buddy/View/Author/SelectInstalledPluginForm.cs
+++ SC4Buddy/View/Author/SelectInstalledPluginForm.cs
@@ -1,20 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace NIHEI.SC4Buddy.View.Author
+﻿namespace NIHEI.SC4Buddy.View.Author
 {
+    using System;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    using NIHEI.SC4Buddy.DataAccess;
+    using NIHEI.SC4Buddy.Entities;
+    using NIHEI.SC4Buddy.View.Elements;
+
     public partial class SelectInstalledPluginForm : Form
     {
+        private readonly UserFolderRegistry userFolderRegistry;
+
         public SelectInstalledPluginForm()
         {
+            userFolderRegistry = RegistryFactory.UserFolderRegistry;
+
             InitializeComponent();
+        }
+
+        private void SelectInstalledPluginFormLoad(object sender, EventArgs e)
+        {
+            var userFolders = userFolderRegistry.UserFolders.Where(x => x.Plugin.Any());
+
+            foreach (var userFolder in userFolders)
+            {
+                userFolderComboBox.Items.Add(new ComboBoxItem<UserFolder>(userFolder.Alias, userFolder));
+            }
         }
     }
 }
