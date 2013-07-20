29addd903e9d9628d2bfb701f45e5283aea953e1
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-20 21:23:42 +0200

    added search functionality to dependenciesform

--- SC4Buddy/View/Author/DependenciesForm.cs
+++ SC4Buddy/View/Author/DependenciesForm.cs
@@ -1,20 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace NIHEI.SC4Buddy.View.Author
+﻿namespace NIHEI.SC4Buddy.View.Author
 {
+    using System;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    using NIHEI.Common.UI.Elements;
+    using NIHEI.SC4Buddy.DataAccess.Remote;
+    using NIHEI.SC4Buddy.Entities.Remote;
+
     public partial class DependenciesForm : Form
     {
+        private readonly RemotePluginRegistry remotePluginRegistry;
+
         public DependenciesForm()
         {
+            remotePluginRegistry = RemoteRegistryFactory.RemotePluginRegistry;
+
             InitializeComponent();
+        }
+
+        private void SearchBoxTextChanged(object sender, EventArgs e)
+        {
+            var text = searchBox.Text.Trim().ToUpper().Replace("//WWW.", "//");
+
+            if (text.Length < 3)
+            {
+                return;
+            }
+
+            var plugins =
+                remotePluginRegistry.RemotePlugins.Where(x
+                    => x.Name.ToUpper().Contains(text)
+                    || x.Author.Name.ToUpper().Contains(text)
+                    || x.Link.ToUpper().Replace("//WWW.", "//").Contains(text));
+
+            searchResultListView.BeginUpdate();
+            searchResultListView.Items.Clear();
+            foreach (var remotePlugin in plugins)
+            {
+                var item = new ListViewItemWithObjectValue<RemotePlugin>(remotePlugin.Name, remotePlugin);
+                item.SubItems.Add(remotePlugin.Author.Name);
+                item.SubItems.Add(remotePlugin.Link);
+            }
+
+            searchResultListView.EndUpdate();
+            searchResultListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
         }
     }
 }
