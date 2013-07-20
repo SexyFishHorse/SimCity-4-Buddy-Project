78368d4b9a1467f4d9fc163c3ef7307ef3fe00ff
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-20 18:59:59 +0200

    added fields and buttons to selectinstalledplugin form

--- SC4Buddy/View/Author/SelectInstalledPluginForm.Designer.cs
+++ SC4Buddy/View/Author/SelectInstalledPluginForm.Designer.cs
@@ -28,11 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userFolderComboBox = new System.Windows.Forms.ComboBox();
+            this.pluginComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.includeInformationCheckBox = new System.Windows.Forms.CheckBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User folder";
+            // 
+            // userFolderComboBox
+            // 
+            this.userFolderComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userFolderComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.userFolderComboBox.FormattingEnabled = true;
+            this.userFolderComboBox.Location = new System.Drawing.Point(76, 12);
+            this.userFolderComboBox.Name = "userFolderComboBox";
+            this.userFolderComboBox.Size = new System.Drawing.Size(339, 21);
+            this.userFolderComboBox.TabIndex = 1;
+            // 
+            // pluginComboBox
+            // 
+            this.pluginComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pluginComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.pluginComboBox.FormattingEnabled = true;
+            this.pluginComboBox.Location = new System.Drawing.Point(76, 39);
+            this.pluginComboBox.Name = "pluginComboBox";
+            this.pluginComboBox.Size = new System.Drawing.Size(339, 21);
+            this.pluginComboBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Plugin";
+            // 
+            // includeInformationCheckBox
+            // 
+            this.includeInformationCheckBox.AutoSize = true;
+            this.includeInformationCheckBox.Location = new System.Drawing.Point(12, 70);
+            this.includeInformationCheckBox.Name = "includeInformationCheckBox";
+            this.includeInformationCheckBox.Size = new System.Drawing.Size(154, 17);
+            this.includeInformationCheckBox.TabIndex = 4;
+            this.includeInformationCheckBox.Text = "Include entered information";
+            this.includeInformationCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(259, 66);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 5;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(340, 66);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 6;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // SelectInstalledPluginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "SelectInstalledPluginForm";
+            this.ClientSize = new System.Drawing.Size(427, 101);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.includeInformationCheckBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pluginComboBox);
+            this.Controls.Add(this.userFolderComboBox);
+            this.Controls.Add(this.label1);
+            this.Name = "SelectInstalledPluginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Select installed plugin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox userFolderComboBox;
+        private System.Windows.Forms.ComboBox pluginComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox includeInformationCheckBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
     }
 }
