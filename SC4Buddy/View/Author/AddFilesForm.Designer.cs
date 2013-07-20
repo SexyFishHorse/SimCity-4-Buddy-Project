39fc76a89abbda11eb621008a53f792ab2f9b946
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-20 18:32:40 +0200

    resized the add files dialog

--- SC4Buddy/View/Author/AddFilesForm.Designer.cs
+++ SC4Buddy/View/Author/AddFilesForm.Designer.cs
@@ -42,7 +42,8 @@ namespace NIHEI.SC4Buddy.View.Author
             // cancelButton
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cancelButton.Location = new System.Drawing.Point(342, 262);
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(453, 357);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 0;
@@ -55,7 +56,7 @@ namespace NIHEI.SC4Buddy.View.Author
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.okButton.Enabled = false;
-            this.okButton.Location = new System.Drawing.Point(261, 262);
+            this.okButton.Location = new System.Drawing.Point(372, 357);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 1;
@@ -72,7 +73,7 @@ namespace NIHEI.SC4Buddy.View.Author
             this.columnHeader2});
             this.filesListView.Location = new System.Drawing.Point(12, 12);
             this.filesListView.Name = "filesListView";
-            this.filesListView.Size = new System.Drawing.Size(405, 244);
+            this.filesListView.Size = new System.Drawing.Size(516, 339);
             this.filesListView.TabIndex = 2;
             this.filesListView.UseCompatibleStateImageBehavior = false;
             this.filesListView.View = System.Windows.Forms.View.Details;
@@ -89,7 +90,7 @@ namespace NIHEI.SC4Buddy.View.Author
             // addButton
             // 
             this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.addButton.Location = new System.Drawing.Point(12, 262);
+            this.addButton.Location = new System.Drawing.Point(12, 357);
             this.addButton.Name = "addButton";
             this.addButton.Size = new System.Drawing.Size(75, 23);
             this.addButton.TabIndex = 3;
@@ -101,7 +102,7 @@ namespace NIHEI.SC4Buddy.View.Author
             // 
             this.removeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.removeButton.Enabled = false;
-            this.removeButton.Location = new System.Drawing.Point(93, 262);
+            this.removeButton.Location = new System.Drawing.Point(93, 357);
             this.removeButton.Name = "removeButton";
             this.removeButton.Size = new System.Drawing.Size(75, 23);
             this.removeButton.TabIndex = 4;
@@ -122,7 +123,7 @@ namespace NIHEI.SC4Buddy.View.Author
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(429, 297);
+            this.ClientSize = new System.Drawing.Size(540, 392);
             this.Controls.Add(this.removeButton);
             this.Controls.Add(this.addButton);
             this.Controls.Add(this.filesListView);
