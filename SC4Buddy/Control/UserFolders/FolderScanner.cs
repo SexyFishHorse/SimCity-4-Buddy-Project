5768459248b9076ebc9b249e0859093db6598cc9
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-23 01:39:43 +0200

    removed unnecessary path.combine

--- SC4Buddy/Control/UserFolders/FolderScanner.cs
+++ SC4Buddy/Control/UserFolders/FolderScanner.cs
@@ -42,7 +42,7 @@
 
         private IEnumerable<string> GetFiles()
         {
-            return Directory.EnumerateFiles(Path.Combine(UserFolder.PluginFolderPath), "*", SearchOption.AllDirectories)
+            return Directory.EnumerateFiles(UserFolder.PluginFolderPath, "*", SearchOption.AllDirectories)
                              .Where(BaseHandler.IsPluginFile);
         }
     }
