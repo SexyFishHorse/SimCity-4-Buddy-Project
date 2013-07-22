4275a51956838ba20668425796784419dcafd72f
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-22 02:50:36 +0200

    show results on search now implemented

--- SC4Buddy/View/Author/UpdatePluginInformationForm.cs
+++ SC4Buddy/View/Author/UpdatePluginInformationForm.cs
@@ -1,20 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace NIHEI.SC4Buddy.View.Author
+﻿namespace NIHEI.SC4Buddy.View.Author
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    using NIHEI.Common.UI.Elements;
+    using NIHEI.SC4Buddy.DataAccess.Remote;
+    using NIHEI.SC4Buddy.Entities.Remote;
+
     public partial class UpdatePluginInformationForm : Form
     {
+        private readonly RemotePluginRegistry remotePluginRegistry;
+
         public UpdatePluginInformationForm()
         {
+            remotePluginRegistry = RemoteRegistryFactory.RemotePluginRegistry;
+
             InitializeComponent();
+        }
+
+        private void UpdateSearchResultListView(IEnumerable<RemotePlugin> plugins)
+        {
+            searchResultsListView.BeginUpdate();
+            searchResultsListView.Items.Clear();
+
+            foreach (var plugin in plugins)
+            {
+                var item = new ListViewItemWithObjectValue<RemotePlugin>(plugin.Name, plugin);
+                item.SubItems.Add(plugin.Author.Name);
+                item.SubItems.Add(plugin.Link);
+            }
+
+            searchResultsListView.EndUpdate();
+        }
+
+        private void SearchTextBoxTextChanged(object sender, EventArgs e)
+        {
+            var text = searchTextBox.Text.Trim().ToUpper().Replace("//WWW.", "//");
+
+            if (text.Length < 3)
+            {
+                return;
+            }
+
+            var plugins =
+                remotePluginRegistry.RemotePlugins.Where(
+                    x =>
+                    x.Name.ToUpper().Contains(text) || x.Link.ToUpper().Replace("//WWW.", "//").Contains(text)
+                    || x.Author.Name.ToUpper().Contains(text));
+
+            UpdateSearchResultListView(plugins);
         }
     }
 }
