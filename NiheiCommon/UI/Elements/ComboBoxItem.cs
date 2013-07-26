2413a6bd634f730de0f5286f350d0e3b0ec7f6e6
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-26 23:34:42 +0200

    added nihei commons project to solution folder

---
+++ NiheiCommon/UI/Elements/ComboBoxItem.cs
@@ -0,0 +1,20 @@
+﻿namespace NIHEI.SC4Buddy.View.Elements
+{
+    public class ComboBoxItem<T>
+    {
+        public ComboBoxItem(string text, T value)
+        {
+            Text = text;
+            Value = value;
+        }
+
+        public string Text { get; set; }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
---
+++ NiheiCommon/UI/Elements/ListViewItemWithObjectValue.cs
@@ -0,0 +1,15 @@
+﻿namespace NIHEI.Common.UI.Elements
+{
+    using System.Windows.Forms;
+
+    public class ListViewItemWithObjectValue<T> : ListViewItem
+    {
+        public ListViewItemWithObjectValue(string text, T value)
+            : base(text)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+    }
+}
