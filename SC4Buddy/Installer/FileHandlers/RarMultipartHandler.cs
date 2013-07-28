45941456de8ac1c2f09e107ccf4e80c11090b0f3
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-28 22:43:43 +0200

    added rarmultipart class

---
+++ SC4Buddy/Installer/FileHandlers/RarMultipartHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace NIHEI.SC4Buddy.Installer.FileHandlers
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class RarMultipartHandler : BaseHandler
+    {
+        public override string RequiredExtension
+        {
+            get
+            {
+                return ".rar";
+            }
+        }
+
+        public override IEnumerable<FileSystemInfo> ExtractFilesToTemp()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
