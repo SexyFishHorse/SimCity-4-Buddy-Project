c7a5d0787aff21f7d216a72b535dd30985dd478b
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-29 20:59:48 +0200

    added constants to test to archive and folders

--- UnitTestProject/Installer/FileHandlers/InstallHandlerTest.cs
+++ UnitTestProject/Installer/FileHandlers/InstallHandlerTest.cs
@@ -1,12 +1,21 @@
 ﻿namespace NIHEI.SC4Buddy.Installer.FileHandlers
 {
+    using System.IO;
+
     using Xunit;
 
     public class InstallHandlerTest
     {
+        private const string PathToTestMaterial = @"..\..\TEST";
+
+        private readonly string archivePath = Path.Combine(PathToTestMaterial, @"multirar\archive.part1.rar");
+
+        private readonly string outputFolder = Path.Combine(PathToTestMaterial, @"multirar\Output");
+
+        private readonly string tempFolder = Path.Combine(PathToTestMaterial, @"multirar\Temp");
 
         [Fact]
-        public void ExtractFilesToTempTest()
+        public void ExtractFilesToTempTest1()
         {
 
         }
