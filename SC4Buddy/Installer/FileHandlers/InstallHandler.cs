45e80b58c6931ccc9a63bc80db9b039778ec4ed5
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-29 15:53:46 +0200

    added installhandler that can handle all archive filetypes

---
+++ SC4Buddy/Installer/FileHandlers/InstallHandler.cs
@@ -0,0 +1,58 @@
+﻿namespace NIHEI.SC4Buddy.Installer.FileHandlers
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    using SharpCompress.Archive;
+    using SharpCompress.Archive.Rar;
+
+    public class InstallHandler : BaseHandler
+    {
+        public override string RequiredExtension
+        {
+            get
+            {
+                return "*";
+            }
+        }
+
+        public override IEnumerable<FileSystemInfo> ExtractFilesToTemp()
+        {
+            CheckFileInfoIsSet();
+            CreateTempFolder();
+
+            var tempEntries = new List<FileSystemInfo>();
+
+            using (var archive = ArchiveFactory.Open(FileInfo))
+            {
+                if (archive is RarArchive)
+                {
+                    var rarArchive = archive as RarArchive;
+
+                    if (rarArchive.IsMultipartVolume() && !rarArchive.IsFirstVolume())
+                    {
+                        return tempEntries;
+                    }
+                }
+
+                foreach (var entry in archive.Entries)
+                {
+                    var tempPath = Path.Combine(TempFolder, entry.FilePath);
+
+                    if (entry.IsDirectory)
+                    {
+                        Directory.CreateDirectory(tempPath);
+                        tempEntries.Add(new DirectoryInfo(tempPath));
+                    }
+                    else
+                    {
+                        entry.WriteToFile(tempPath);
+                        tempEntries.Add(new FileInfo(tempPath));
+                    }
+                }
+            }
+
+            return tempEntries;
+        }
+    }
+}
