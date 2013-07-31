f0dd3e76a7a1a2b8b19eea2414bbc17d26be87a0
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-31 08:08:19 +0200

    added selected item changed in list view

--- SC4Buddy/View/Plugins/MissingDependenciesForm.cs
+++ SC4Buddy/View/Plugins/MissingDependenciesForm.cs
@@ -1,20 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NIHEI.SC4Buddy.View.Plugins
 {
+    using NIHEI.Common.UI.Elements;
+
     public partial class MissingDependenciesForm : Form
     {
+        private string link;
+
         public MissingDependenciesForm()
         {
             InitializeComponent();
+        }
+
+        private void DependencyListViewSelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (dependencyListView.SelectedItems.Count > 0)
+            {
+                var item =
+                    ((ListViewItemWithObjectValue<Entities.Remote.RemotePlugin>)dependencyListView.SelectedItems[0])
+                        .Value;
+
+                link = item.Link;
+
+                goToDownloadButton.Enabled = true;
+            }
+            else
+            {
+                goToDownloadButton.Enabled = false;
+            }
         }
     }
 }
