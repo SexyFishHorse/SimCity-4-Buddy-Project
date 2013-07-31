be129dd6ba887b59a8a5f9cfa0517d6d4503c653
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-31 07:44:31 +0200

    added logic to check for missing dependencies in curent user folder

--- SC4Buddy/View/UserFolders/DependencyChecker.cs
+++ SC4Buddy/View/UserFolders/DependencyChecker.cs
@@ -1,12 +1,35 @@
 ﻿namespace NIHEI.SC4Buddy.View.UserFolders
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using NIHEI.SC4Buddy.DataAccess.Remote;
     using NIHEI.SC4Buddy.Entities;
+    using NIHEI.SC4Buddy.Entities.Remote;
 
     public class DependencyChecker
     {
-        public void CheckDependencies(UserFolder userFolder)
+        public List<RemotePlugin> CheckDependencies(UserFolder userFolder)
         {
-            throw new System.NotImplementedException();
+            var knownPlugins = userFolder.Plugins.Where(x => x.RemotePluginId > 0).ToList();
+
+            var remotePluginRegistry = RemoteRegistryFactory.RemotePluginRegistry;
+
+            var remotePluginsWithDependencies = new List<RemotePlugin>(knownPlugins.Count());
+            remotePluginsWithDependencies.AddRange(
+                knownPlugins.Select(
+                    knownPlugin =>
+                    remotePluginRegistry.RemotePlugins.FirstOrDefault(
+                        x => x.Id == knownPlugin.RemotePluginId && x.Dependencies.Any())));
+
+            var missingDependencies =
+                (remotePluginsWithDependencies.SelectMany(
+                    remotePlugin => remotePlugin.Dependencies,
+                    (remotePlugin, dependency) => new { remotePlugin, dependency })
+                                              .Where(@t => knownPlugins.All(x => x.RemotePluginId != @t.dependency.Id))
+                                              .Select(@t => @t.dependency)).ToList();
+
+            return missingDependencies;
         }
     }
 }
