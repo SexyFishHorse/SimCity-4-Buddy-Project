716577742de85f269631e796444112cd0a5569ec
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-31 08:05:58 +0200

    added layout to missing dependencies form

--- SC4Buddy/View/Plugins/MissingDependenciesForm.Designer.cs
+++ SC4Buddy/View/Plugins/MissingDependenciesForm.Designer.cs
@@ -28,18 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MissingDependenciesForm));
+            this.dependencyListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.goToDownloadButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // dependencyListView
+            // 
+            this.dependencyListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dependencyListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.dependencyListView.Location = new System.Drawing.Point(12, 25);
+            this.dependencyListView.MultiSelect = false;
+            this.dependencyListView.Name = "dependencyListView";
+            this.dependencyListView.Size = new System.Drawing.Size(546, 372);
+            this.dependencyListView.TabIndex = 0;
+            this.dependencyListView.UseCompatibleStateImageBehavior = false;
+            this.dependencyListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Author";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Link";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "You are missing the following dependencies";
+            // 
+            // goToDownloadButton
+            // 
+            this.goToDownloadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.goToDownloadButton.Enabled = false;
+            this.goToDownloadButton.Location = new System.Drawing.Point(12, 403);
+            this.goToDownloadButton.Name = "goToDownloadButton";
+            this.goToDownloadButton.Size = new System.Drawing.Size(113, 23);
+            this.goToDownloadButton.TabIndex = 2;
+            this.goToDownloadButton.Text = "Go to download";
+            this.goToDownloadButton.UseVisualStyleBackColor = true;
+            // 
+            // closeButton
+            // 
+            this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.closeButton.Location = new System.Drawing.Point(483, 403);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
             // 
             // MissingDependenciesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.CancelButton = this.closeButton;
+            this.ClientSize = new System.Drawing.Size(570, 438);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.goToDownloadButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dependencyListView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MissingDependenciesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Missing dependencies";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView dependencyListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button goToDownloadButton;
+        private System.Windows.Forms.Button closeButton;
     }
 }
