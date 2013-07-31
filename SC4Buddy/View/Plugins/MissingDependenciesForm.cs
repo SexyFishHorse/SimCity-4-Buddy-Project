74b30efbbc85b8f12df65b838830a6a975bd1198
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-31 09:56:36 +0200

    added missingdependencies list

--- SC4Buddy/View/Plugins/MissingDependenciesForm.cs
+++ SC4Buddy/View/Plugins/MissingDependenciesForm.cs
@@ -3,17 +3,32 @@ using System.Windows.Forms;
 
 namespace NIHEI.SC4Buddy.View.Plugins
 {
+    using System.Collections.Generic;
     using System.Diagnostics;
 
     using NIHEI.Common.UI.Elements;
+    using NIHEI.SC4Buddy.Entities.Remote;
+    using NIHEI.SC4Buddy.Localization;
 
     public partial class MissingDependenciesForm : Form
     {
+        private IEnumerable<RemotePlugin> missingDependencies;
         private string link;
 
         public MissingDependenciesForm()
         {
             InitializeComponent();
+        }
+
+        public IEnumerable<RemotePlugin> MissingDependencies
+        {
+            set
+            {
+                missingDependencies = value;
+                visitedDependencies = new List<RemotePlugin>();
+
+                UpdateListView();
+            }
         }
 
         private void DependencyListViewSelectedIndexChanged(object sender, EventArgs e)
