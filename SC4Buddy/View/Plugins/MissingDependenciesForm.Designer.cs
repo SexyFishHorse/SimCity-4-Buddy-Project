7cdb69477f824c4d59863b63c468fb1885667877
Asbjørn Hansen <asb.han@nihei.dk>
2013-07-31 08:00:44 +0200

    added missing dependencies form

---
+++ SC4Buddy/View/Plugins/MissingDependenciesForm.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace NIHEI.SC4Buddy.View.Plugins
+{
+    partial class MissingDependenciesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MissingDependenciesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "MissingDependenciesForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
