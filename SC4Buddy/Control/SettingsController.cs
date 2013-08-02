154263b6671ec6d45b6727237547acefbc073fe7
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-02 22:37:48 +0200

    added validate game location path and added nullcheck

--- SC4Buddy/Control/SettingsController.cs
+++ SC4Buddy/Control/SettingsController.cs
@@ -1,6 +1,18 @@
 ﻿namespace NIHEI.SC4Buddy.Control
 {
+    using System;
+    using System.IO;
+
     public class SettingsController
     {
+        public bool ValidateGameLocationPath(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException(@"Path cannot be null or empty", "path");
+            }
+
+            return File.Exists(Path.Combine(path, @"Apps\SimCity 4.exe"));
+        }
     }
 }
