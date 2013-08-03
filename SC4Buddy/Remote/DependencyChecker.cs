84b2c568e7531a94eaba7e242c85180387a387cb
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-03 05:55:53 +0200

    dependency checker now checks main folder correctly

--- SC4Buddy/Remote/DependencyChecker.cs
+++ SC4Buddy/Remote/DependencyChecker.cs
@@ -13,26 +13,42 @@
     {
         public List<RemotePlugin> CheckDependencies(UserFolder userFolder)
         {
-            var missingDependencies = GetMissingDependenciesForFolder(userFolder);
+            var knownPluginsWithDependencies = GetPluginsWithDependencies(userFolder);
+            var knownPlugins = GetKnownPlugins(userFolder);
+
+            var missingDependencies = GetMissingDependencies(knownPluginsWithDependencies, knownPlugins);
 
             var controller = new UserFolderController(RegistryFactory.UserFolderRegistry);
 
             if (!controller.IsMainFolder(userFolder))
             {
-                var mainMissingDependencies = GetMissingDependenciesForFolder(controller.GetMainUserFolder());
+                var knownMainPlugins = GetKnownPlugins(controller.GetMainUserFolder());
 
-                var extraMissing = mainMissingDependencies.Where(mainMissing => !missingDependencies.Any()).ToList();
+                var mainMissingDependencies = GetMissingDependencies(
+                    knownPluginsWithDependencies, knownMainPlugins);
 
-                missingDependencies.AddRange(extraMissing);
+                var commonMissing = new List<RemotePlugin>();
+                foreach (var missingDependency in missingDependencies)
+                {
+                    foreach (var mainMissingDependency in mainMissingDependencies)
+                    {
+                        if (missingDependency.Id == mainMissingDependency.Id)
+                        {
+                            commonMissing.Add(missingDependency);
+                        }
+                    }
+                }
+
+                missingDependencies = commonMissing;
             }
 
             return missingDependencies;
         }
 
-        private static List<RemotePlugin> GetMissingDependenciesForFolder(UserFolder userFolder)
+        private static List<RemotePlugin> GetPluginsWithDependencies(UserFolder userFolder)
         {
             var knownPlugins =
-                userFolder.Plugins.Where(x => x.RemotePluginId > 0 && x.UserFolder.Equals(userFolder)).ToList();
+                userFolder.Plugins.Where(x => x.RemotePluginId > 0 && x.UserFolder.Id == userFolder.Id).ToList();
 
             var remotePluginRegistry = RemoteRegistryFactory.RemotePluginRegistry;
 
@@ -52,20 +68,54 @@
                 }
             }
 
-            var missingDependencies = new List<RemotePlugin>();
+            return remotePluginsWithDependencies;
+        }
 
-            foreach (var remotePlugin in remotePluginsWithDependencies)
+        private static List<RemotePlugin> GetKnownPlugins(UserFolder userFolder)
+        {
+            var knownPlugins =
+                userFolder.Plugins.Where(x => x.RemotePluginId > 0 && x.UserFolder.Id == userFolder.Id).ToList();
+
+            var remotePluginRegistry = RemoteRegistryFactory.RemotePluginRegistry;
+
+            var remotePluginsWithDependencies = new List<RemotePlugin>();
+
+            foreach (var knownPlugin in knownPlugins)
             {
-                foreach (var dependency in remotePlugin.Dependencies)
+                foreach (var remotePlugin in remotePluginRegistry.RemotePlugins.Include("Author"))
                 {
-                    if (!knownPlugins.Any(x => x.RemotePluginId == dependency.Id))
+                    if (remotePlugin.Id == knownPlugin.RemotePluginId)
                     {
-                        missingDependencies.Add(dependency);
+                        remotePluginsWithDependencies.Add(remotePlugin);
                     }
                 }
             }
 
-            return missingDependencies;
+            return remotePluginsWithDependencies;
+        }
+
+        private static List<RemotePlugin> GetMissingDependencies(List<RemotePlugin> installedPlugins, List<RemotePlugin> pluginsToCheckAgainst)
+        {
+            var allDependencies = installedPlugins.SelectMany(x => x.Dependencies).ToList();
+
+            var installedDependency = new List<RemotePlugin>();
+            foreach (RemotePlugin dependency in allDependencies)
+            {
+                foreach (RemotePlugin plugin in pluginsToCheckAgainst)
+                {
+                    if (dependency.Equals(plugin))
+                    {
+                        installedDependency.Add(dependency);
+                    }
+                }
+            }
+
+            foreach (var remotePlugin in installedDependency)
+            {
+                allDependencies.Remove(remotePlugin);
+            }
+
+            return allDependencies;
         }
     }
 }
