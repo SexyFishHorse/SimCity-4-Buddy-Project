197e9c2d5f50f07735a1bb21598dfdb3bc8f80af
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-03 05:13:12 +0200

    added userfolder check for get  missing dependencies for folder

--- SC4Buddy/Remote/DependencyChecker.cs
+++ SC4Buddy/Remote/DependencyChecker.cs
@@ -31,7 +31,7 @@
 
         private static List<RemotePlugin> GetMissingDependenciesForFolder(UserFolder userFolder)
         {
-            var knownPlugins = userFolder.Plugins.Where(x => x.RemotePluginId > 0).ToList();
+            var knownPlugins = userFolder.Plugins.Where(x => x.RemotePluginId > 0 && x.UserFolder.Equals(userFolder)).ToList();
 
             var remotePluginRegistry = RemoteRegistryFactory.RemotePluginRegistry;
 
