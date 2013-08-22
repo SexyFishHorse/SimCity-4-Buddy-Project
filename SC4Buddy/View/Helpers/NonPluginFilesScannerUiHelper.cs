5f65c33123f877814c601022626ce9f0a83dcbb7
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-21 17:25:01 -1000

    added show dialog methods to new helper class

--- SC4Buddy/View/Helpers/NonPluginFilesScannerUiHelper.cs
+++ SC4Buddy/View/Helpers/NonPluginFilesScannerUiHelper.cs
@@ -1,6 +1,44 @@
 ﻿namespace NIHEI.SC4Buddy.View.Helpers
 {
+    using System.Windows.Forms;
+
+    using NIHEI.SC4Buddy.Localization;
+    using NIHEI.SC4Buddy.View.UserFolders;
+
     public static class NonPluginFilesScannerUiHelper
     {
+        public static void ShowThereAreNoEntitiesToRemoveDialog(Form parentForm)
+        {
+            MessageBox.Show(
+                    parentForm,
+                    LocalizationStrings.ThereAreNoNonPluginFilesOrEmptyFoldersToRemove,
+                    LocalizationStrings.NoNonPluginFilesDetected,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+        }
+
+        public static bool ShowConfirmDialog(UserFolderForm parentForm, int numFiles, int numFolders)
+        {
+            return MessageBox.Show(
+                parentForm,
+                string.Format(LocalizationStrings.ThisWillRemoveNumFilesAndAtLeastNumFolders, numFiles, numFolders),
+                LocalizationStrings.ConfirmDeletionOfNonPluginFiles,
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2) == DialogResult.Yes;
+        }
+
+        public static void ShowRemovalSummary(UserFolderForm parentForm, int numFiles, int numFolders)
+        {
+            var message = string.Format(LocalizationStrings.NumFilesAndNumFoldersWereRemoved, numFiles, numFolders);
+
+            MessageBox.Show(
+                parentForm,
+                message,
+                LocalizationStrings.NonPluginFilesDeleted,
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1);
+        }
     }
 }
