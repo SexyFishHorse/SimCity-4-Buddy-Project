ee7ddd2dbc6bbc1adae3d339ccd0a376376351b0
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-24 11:15:41 +0200

    added plugin property to move or copy form

--- SC4Buddy/View/Plugins/MoveOrCopyForm.cs
+++ SC4Buddy/View/Plugins/MoveOrCopyForm.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace NIHEI.SC4Buddy.View.Plugins
 {
+    using NIHEI.SC4Buddy.Entities;
+
     public partial class MoveOrCopyForm : Form
     {
         public MoveOrCopyForm()
         {
             InitializeComponent();
         }
+
+        public Plugin Plugin { get; set; }
     }
 }
