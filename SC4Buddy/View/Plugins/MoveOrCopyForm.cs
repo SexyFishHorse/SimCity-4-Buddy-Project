dd814d518006b0e86f25bdc6ddca70690733271f
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-24 11:05:35 +0200

    added move or copy form

---
+++ SC4Buddy/View/Plugins/MoveOrCopyForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NIHEI.SC4Buddy.View.Plugins
+{
+    public partial class MoveOrCopyForm : Form
+    {
+        public MoveOrCopyForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
