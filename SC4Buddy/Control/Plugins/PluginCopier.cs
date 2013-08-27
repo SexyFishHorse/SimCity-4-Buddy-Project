40b2a343f8794afd8635988d02f9040acad3b33d
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-27 17:06:29 +0200

    added copyfile method to plugin copier

--- SC4Buddy/Control/Plugins/PluginCopier.cs
+++ SC4Buddy/Control/Plugins/PluginCopier.cs
@@ -1,6 +1,32 @@
 ﻿namespace NIHEI.SC4Buddy.Control.Plugins
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using NIHEI.SC4Buddy.DataAccess;
+    using NIHEI.SC4Buddy.Entities;
     public class PluginCopier
     {
+        private PluginFile CopyFile(PluginFile pluginFile, UserFolder targetUserFolder)
+        {
+            var currentPath = pluginFile.Path;
+            var relativeFilePath = currentPath.Remove(0, pluginFile.Plugin.UserFolder.PluginFolderPath.Length + 2);
+            var newFilePath = Path.Combine(targetUserFolder.PluginFolderPath, relativeFilePath);
+            var newDirectoryPath = targetUserFolder.PluginFolderPath;
+            if (relativeFilePath.Contains("\\"))
+            {
+                newDirectoryPath = Path.Combine(
+                    newDirectoryPath,
+                    relativeFilePath.Remove(relativeFilePath.LastIndexOf("\\", StringComparison.OrdinalIgnoreCase)));
+            }
+
+            Directory.CreateDirectory(newDirectoryPath);
+            File.Copy(currentPath, newFilePath, true);
+
+            var file = new PluginFile { Checksum = pluginFile.Checksum, Path = newFilePath };
+            return file;
+        }
     }
 }
