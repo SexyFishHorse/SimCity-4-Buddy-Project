fb6ba6984565474995a525858afe391551e4bbd6
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-28 17:43:41 +0200

    added logic to authorcontroller

--- SC4Buddy/Control/Remote/AuthorController.cs
+++ SC4Buddy/Control/Remote/AuthorController.cs
@@ -1,6 +1,54 @@
 ﻿namespace NIHEI.SC4Buddy.Control.Remote
 {
+    using System;
+    using System.Data.Objects;
+    using System.Linq;
+
+    using NIHEI.SC4Buddy.DataAccess;
+    using NIHEI.SC4Buddy.Entities.Remote;
+
     public class AuthorController
     {
+        private readonly IRemoteEntitites entities;
+
+        public AuthorController(IRemoteEntitites entities)
+        {
+            this.entities = entities;
+        }
+
+        public IObjectSet<Author> Authors
+        {
+            get
+            {
+                return entities.Authors;
+            }
+        }
+
+        public Author GetAuthorByName(string authorName)
+        {
+            if (!Authors.Any(x => x.Name.Equals(authorName, StringComparison.OrdinalIgnoreCase)))
+            {
+                return null;
+            }
+
+            return Authors.First(x => x.Name.Equals(authorName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void Add(Author author)
+        {
+            entities.Authors.AddObject(author);
+            SaveChanges();
+        }
+
+        public void Delete(Author author)
+        {
+            entities.Authors.DeleteObject(author);
+            SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            entities.SaveChanges();
+        }
     }
 }
