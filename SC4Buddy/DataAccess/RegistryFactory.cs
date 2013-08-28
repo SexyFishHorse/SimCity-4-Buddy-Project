2c296477df7a9d0494fac4698b7065870af2c971
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-28 17:22:59 +0200

    removed class pluginfileregistry from solution

--- SC4Buddy/DataAccess/RegistryFactory.cs
+++ SC4Buddy/DataAccess/RegistryFactory.cs
@@ -7,23 +7,12 @@
     {
         private static readonly RegistryFactory Instance = new RegistryFactory();
 
-        private readonly PluginFileRegistry pluginFileRegistry;
-
         private readonly PluginGroupRegistry pluginGroupRegistry;
 
         private RegistryFactory()
         {
             var databaseEntities = new DatabaseEntities();
-            pluginFileRegistry = new PluginFileRegistry(databaseEntities);
             pluginGroupRegistry = new PluginGroupRegistry(databaseEntities);
-        }
-
-        public static PluginFileRegistry PluginFileRegistry
-        {
-            get
-            {
-                return Instance.pluginFileRegistry;
-            }
         }
 
         public static PluginGroupRegistry PluginGroupRegistry
