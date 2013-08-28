6602de4cdf6a58164759b9ce82b7be4241d67a91
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-28 20:15:24 +0200

    added logic to remotepluginfilecontroller class

--- SC4Buddy/Control/Remote/RemotePluginFileController.cs
+++ SC4Buddy/Control/Remote/RemotePluginFileController.cs
@@ -1,6 +1,36 @@
 ﻿namespace NIHEI.SC4Buddy.Control.Remote
 {
+    using System.Data.Objects;
+
+    using NIHEI.SC4Buddy.DataAccess;
+    using NIHEI.SC4Buddy.Entities.Remote;
+
     public class RemotePluginFileController
     {
+        private readonly IRemoteEntities entities;
+
+        public RemotePluginFileController(IRemoteEntities entities)
+        {
+            this.entities = entities;
+        }
+
+        public IObjectSet<RemotePluginFile> Files
+        {
+            get
+            {
+                return entities.PluginFiles;
+            }
+        }
+
+        public void Add(RemotePluginFile remoteFile)
+        {
+            Files.AddObject(remoteFile);
+            SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            entities.SaveChanges();
+        }
     }
 }
