b6c2ee8e7fa598d01d4c0af32ee563380d5deb2c
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-28 08:43:35 +0200

    entityfactory is now a singleton5

--- SC4Buddy/DataAccess/EntityFactory.cs
+++ SC4Buddy/DataAccess/EntityFactory.cs
@@ -2,5 +2,19 @@
 {
     public class EntityFactory
     {
+        private static EntityFactory instance;
+
+        public static EntityFactory Instance
+        {
+            get
+            {
+                return instance ?? (instance = new EntityFactory());
+            }
+        }
+
+        private EntityFactory()
+        {
+        }
+        }
     }
 }
