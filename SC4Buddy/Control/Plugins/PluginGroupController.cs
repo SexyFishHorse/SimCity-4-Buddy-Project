6dddd739fc29bf8755fbb9038d8b4ba603b3b3b1
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-28 17:27:18 +0200

    added plugingroupcontroller class

---
+++ SC4Buddy/Control/Plugins/PluginGroupController.cs
@@ -0,0 +1,36 @@
+﻿namespace NIHEI.SC4Buddy.Control.Plugins
+{
+    using System.Data.Objects;
+
+    using NIHEI.SC4Buddy.DataAccess;
+    using NIHEI.SC4Buddy.Entities;
+
+    public class PluginGroupController
+    {
+        private readonly IEntities entities;
+
+        public PluginGroupController(IEntities entities)
+        {
+            this.entities = entities;
+        }
+
+        public IObjectSet<PluginGroup> Groups
+        {
+            get
+            {
+                return entities.Groups;
+            }
+        }
+
+        public void Delete(PluginGroup pluginGroup)
+        {
+            Groups.DeleteObject(pluginGroup);
+            SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            entities.SaveChanges();
+        }
+    }
+}
