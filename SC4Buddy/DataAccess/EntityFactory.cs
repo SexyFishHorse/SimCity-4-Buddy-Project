62b9a9f1927ea7486c1836a1c721f5b6f50a5d01
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-28 08:44:39 +0200

    added createremoteentities method to connect to remote entities database

--- SC4Buddy/DataAccess/EntityFactory.cs
+++ SC4Buddy/DataAccess/EntityFactory.cs
@@ -1,7 +1,11 @@
 ﻿namespace NIHEI.SC4Buddy.DataAccess
 {
+    using System.Configuration;
+    using System.Data.EntityClient;
+
     using NIHEI.SC4Buddy.Entities;
     using NIHEI.SC4Buddy.Entities.Remote;
+
     public class EntityFactory
     {
         private static EntityFactory instance;
@@ -21,6 +25,20 @@
         public Entities Entities { get; private set; }
 
         public RemoteEntities RemoteEntities { get; private set; }
+
+        private RemoteEntities CreateRemoteEntities()
+        {
+            var originalConnectionString = ConfigurationManager.ConnectionStrings["RemoteEntities"].ConnectionString;
+
+            var entityBuilder = new EntityConnectionStringBuilder(originalConnectionString);
+            var providerConnectionString = entityBuilder.ProviderConnectionString;
+            providerConnectionString += ";password=_VslefXPl5Tg8pBcSYzI";
+
+            entityBuilder.ProviderConnectionString = providerConnectionString;
+
+            var entities = new RemoteDatabaseEntities(entityBuilder.ConnectionString);
+
+            return new RemoteEntities(entities);
         }
     }
 }
