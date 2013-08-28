b7e2c2dc1fbcb8ddbef1151abc2435a2647fd2bf
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-28 08:29:48 +0200

    implemented interface members from iremoteentities

--- SC4Buddy/DataAccess/RemoteEntities.cs
+++ SC4Buddy/DataAccess/RemoteEntities.cs
@@ -2,8 +2,19 @@
 {
     using System.Linq;
 
-    public class RemoteEntities
+    public class RemoteEntities : IRemoteEntitites
     {
-        
+        public IQueryable<Author> Authors { get; private set; }
+
+        public IQueryable<RemotePlugin> Plugins { get; private set; }
+
+        public IQueryable<RemotePluginFile> PluginFiles { get; private set; }
+
+        public IQueryable<User> Users { get; private set; }
+
+        public void SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
