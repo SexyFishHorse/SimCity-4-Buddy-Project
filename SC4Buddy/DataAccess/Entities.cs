f4f06964ea88f869f52b95815ce60260e8bab92f
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-28 08:35:06 +0200

    implemented members from ientities interface

--- SC4Buddy/DataAccess/Entities.cs
+++ SC4Buddy/DataAccess/Entities.cs
@@ -4,7 +4,19 @@
 
     using NIHEI.SC4Buddy.Entities;
 
-    public class Entities
+    public class Entities : IEntities
     {
+        public IQueryable<Plugin> Plugins { get; private set; }
+
+        public IQueryable<PluginFile> Files { get; private set; }
+
+        public IQueryable<UserFolder> UserFolders { get; private set; }
+
+        public IQueryable<PluginGroup> Groups { get; private set; }
+
+        public void SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
