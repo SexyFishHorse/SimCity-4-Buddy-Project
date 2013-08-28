66592dd6b339476e0b1b06535c69c376d9786fe3
Asbjørn Hansen <asb.han@nihei.dk>
2013-08-28 08:44:16 +0200

    added properties for entities and remote entities

--- SC4Buddy/DataAccess/EntityFactory.cs
+++ SC4Buddy/DataAccess/EntityFactory.cs
@@ -1,5 +1,7 @@
 ﻿namespace NIHEI.SC4Buddy.DataAccess
 {
+    using NIHEI.SC4Buddy.Entities;
+    using NIHEI.SC4Buddy.Entities.Remote;
     public class EntityFactory
     {
         private static EntityFactory instance;
@@ -15,6 +17,10 @@
         private EntityFactory()
         {
         }
+
+        public Entities Entities { get; private set; }
+
+        public RemoteEntities RemoteEntities { get; private set; }
         }
     }
 }
