40de2e565f05cb88f38f7ca3fd3ff0d25f61531c
Asbjørn Hansen <asb.han@nihei.dk>
2013-09-05 16:32:51 +0200

    listviews are now populated on load

--- SC4Buddy/View/Plugins/QuarantinedPluginFilesForm.cs
+++ SC4Buddy/View/Plugins/QuarantinedPluginFilesForm.cs
@@ -1,4 +1,8 @@
-﻿using NIHEI.SC4Buddy.Entities;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NIHEI.Common.UI.Elements;
+using NIHEI.SC4Buddy.Entities;
 
 namespace NIHEI.SC4Buddy.View.Plugins
 {
@@ -19,6 +23,29 @@ namespace NIHEI.SC4Buddy.View.Plugins
         private void CancelButtonClick(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void QuarantinedPluginFilesForm_Load(object sender, EventArgs e)
+        {
+            var enabledFiles = _selectedPlugin.Files.Where(x => !x.Quarantined.HasValue || !x.Quarantined.Value).ToList();
+            var disabledFiles = _selectedPlugin.Files.Where(x => x.Quarantined.HasValue && x.Quarantined.Value).ToList();
+
+            PopulateListView(activeFilesListView, enabledFiles);
+            PopulateListView(disabledFilesListView, disabledFiles);
+        }
+
+        private void PopulateListView(ListView listView, List<PluginFile> files)
+        {
+            if (files.Any())
+            {
+                listView.BeginUpdate();
+                listView.Items.Clear();
+                foreach (var file in files)
+                {
+                    var filename = new FileInfo(file.Path).Name;
+                    listView.Items.Add(new ListViewItemWithObjectValue<PluginFile>(filename, file));
+                }
+            }
         }
     }
 }
