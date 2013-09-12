38467ac0ca9173c69bbdd9b1cde55b47c84eec1f
Asbjørn Hansen <asb.han@nihei.dk>
2013-09-12 08:38:08 +0200

    added RemotePlugin property to plugin class

--- SC4Buddy/Entities/Plugin.cs
+++ SC4Buddy/Entities/Plugin.cs
@@ -1,6 +1,18 @@
 ﻿namespace NIHEI.SC4Buddy.Entities
 {
+    using System.Linq;
+
+    using NIHEI.SC4Buddy.DataAccess;
+    using NIHEI.SC4Buddy.Entities.Remote;
+
     public partial class Plugin
     {
+        public RemotePlugin RemotePlugin
+        {
+            get
+            {
+                return EntityFactory.Instance.RemoteEntities.Plugins.FirstOrDefault(x => RemotePluginId == x.Id);
+            }
+        }
     }
 }
