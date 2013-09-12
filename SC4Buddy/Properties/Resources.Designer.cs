1a06a7766d23592abdfc509189911c446b6e86c9
Asbjørn Hansen <asb.han@nihei.dk>
2013-09-12 07:38:18 +0200

    added error icon to ressources

--- SC4Buddy/Properties/Resources.Designer.cs
+++ SC4Buddy/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace NIHEI.SC4Buddy.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap IconError {
+            get {
+                object obj = ResourceManager.GetObject("IconError", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap IconZoom {
             get {
                 object obj = ResourceManager.GetObject("IconZoom", resourceCulture);
