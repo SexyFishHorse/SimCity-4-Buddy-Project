dfb74b28023b6ebff841c3497e796cc6d987d211
Asbjørn Hansen <asb.han@nihei.dk>
2013-09-13 19:27:58 +0200

    added layout to report dialog

--- SC4Buddy/View/Plugins/ReportPluginForm.Designer.cs
+++ SC4Buddy/View/Plugins/ReportPluginForm.Designer.cs
@@ -28,11 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportPluginForm));
+            this.reportTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.reportButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reportTextBox
+            // 
+            this.reportTextBox.AcceptsReturn = true;
+            this.reportTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.reportTextBox.Location = new System.Drawing.Point(12, 25);
+            this.reportTextBox.Multiline = true;
+            this.reportTextBox.Name = "reportTextBox";
+            this.reportTextBox.Size = new System.Drawing.Size(378, 219);
+            this.reportTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please enter your report or concern below. Please keep the report in ENGLISH.";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(315, 250);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // reportButton
+            // 
+            this.reportButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.reportButton.Location = new System.Drawing.Point(234, 250);
+            this.reportButton.Name = "reportButton";
+            this.reportButton.Size = new System.Drawing.Size(75, 23);
+            this.reportButton.TabIndex = 3;
+            this.reportButton.Text = "File report";
+            this.reportButton.UseVisualStyleBackColor = true;
+            // 
+            // ReportPluginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ReportPluginForm";
+            this.ClientSize = new System.Drawing.Size(402, 285);
+            this.Controls.Add(this.reportButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reportTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ReportPluginForm";
+            this.Text = "Report plugin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox reportTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button reportButton;
     }
 }
