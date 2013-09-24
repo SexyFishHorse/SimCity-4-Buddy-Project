12af8be49f3b7d91a121be806f225bca2c5500ea
Asbjørn Hansen <asb.han@nihei.dk>
2013-09-24 08:51:40 +0200

    added manage reports form and added layout to new form

--- SC4Buddy/View/Admin/ManageReportsForm.Designer.cs
+++ SC4Buddy/View/Admin/ManageReportsForm.Designer.cs
@@ -28,11 +28,273 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("New", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Active", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("Not active", System.Windows.Forms.HorizontalAlignment.Left);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManageReportsForm));
+            this.numUnhandledReportsLabel = new System.Windows.Forms.Label();
+            this.nextUnhandledButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchComboBox = new System.Windows.Forms.ComboBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.approveCheckBox = new System.Windows.Forms.CheckBox();
+            this.reportsListView = new System.Windows.Forms.ListView();
+            this.reportBodyTextBox = new System.Windows.Forms.TextBox();
+            this.pluginNameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.dateColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numUnhandledReportsLabel
+            // 
+            this.numUnhandledReportsLabel.AutoSize = true;
+            this.numUnhandledReportsLabel.Location = new System.Drawing.Point(12, 17);
+            this.numUnhandledReportsLabel.Name = "numUnhandledReportsLabel";
+            this.numUnhandledReportsLabel.Size = new System.Drawing.Size(121, 13);
+            this.numUnhandledReportsLabel.TabIndex = 0;
+            this.numUnhandledReportsLabel.Text = "[num] unhandled reports";
+            // 
+            // nextUnhandledButton
+            // 
+            this.nextUnhandledButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nextUnhandledButton.Enabled = false;
+            this.nextUnhandledButton.Location = new System.Drawing.Point(583, 12);
+            this.nextUnhandledButton.Name = "nextUnhandledButton";
+            this.nextUnhandledButton.Size = new System.Drawing.Size(75, 23);
+            this.nextUnhandledButton.TabIndex = 1;
+            this.nextUnhandledButton.Text = "Next report";
+            this.nextUnhandledButton.UseVisualStyleBackColor = true;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(583, 41);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Search by plugin";
+            // 
+            // searchComboBox
+            // 
+            this.searchComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.searchComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.searchComboBox.FormattingEnabled = true;
+            this.searchComboBox.Location = new System.Drawing.Point(104, 42);
+            this.searchComboBox.Name = "searchComboBox";
+            this.searchComboBox.Size = new System.Drawing.Size(473, 21);
+            this.searchComboBox.TabIndex = 5;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(583, 276);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.approveCheckBox);
+            this.panel1.Controls.Add(this.reportsListView);
+            this.panel1.Controls.Add(this.reportBodyTextBox);
+            this.panel1.Controls.Add(this.pluginNameLabel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.removeButton);
+            this.panel1.Controls.Add(this.updateButton);
+            this.panel1.Controls.Add(this.addButton);
+            this.panel1.Location = new System.Drawing.Point(0, 70);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0, 3, 0, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(670, 200);
+            this.panel1.TabIndex = 8;
+            // 
+            // approveCheckBox
+            // 
+            this.approveCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.approveCheckBox.AutoSize = true;
+            this.approveCheckBox.Location = new System.Drawing.Point(258, 151);
+            this.approveCheckBox.Name = "approveCheckBox";
+            this.approveCheckBox.Size = new System.Drawing.Size(65, 17);
+            this.approveCheckBox.TabIndex = 8;
+            this.approveCheckBox.Text = "Activate";
+            this.approveCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // reportsListView
+            // 
+            this.reportsListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.reportsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.dateColumnHeader,
+            this.textColumnHeader});
+            listViewGroup1.Header = "New";
+            listViewGroup1.Name = "newListViewGroup";
+            listViewGroup2.Header = "Active";
+            listViewGroup2.Name = "activeListViewGroup";
+            listViewGroup3.Header = "Not active";
+            listViewGroup3.Name = "nonActiveListViewGroup";
+            this.reportsListView.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1,
+            listViewGroup2,
+            listViewGroup3});
+            this.reportsListView.Location = new System.Drawing.Point(12, 16);
+            this.reportsListView.Name = "reportsListView";
+            this.reportsListView.Size = new System.Drawing.Size(237, 152);
+            this.reportsListView.TabIndex = 7;
+            this.reportsListView.UseCompatibleStateImageBehavior = false;
+            this.reportsListView.View = System.Windows.Forms.View.List;
+            // 
+            // reportBodyTextBox
+            // 
+            this.reportBodyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.reportBodyTextBox.Location = new System.Drawing.Point(258, 16);
+            this.reportBodyTextBox.Multiline = true;
+            this.reportBodyTextBox.Name = "reportBodyTextBox";
+            this.reportBodyTextBox.Size = new System.Drawing.Size(400, 129);
+            this.reportBodyTextBox.TabIndex = 6;
+            // 
+            // pluginNameLabel
+            // 
+            this.pluginNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pluginNameLabel.Location = new System.Drawing.Point(255, 0);
+            this.pluginNameLabel.Name = "pluginNameLabel";
+            this.pluginNameLabel.Size = new System.Drawing.Size(403, 13);
+            this.pluginNameLabel.TabIndex = 5;
+            this.pluginNameLabel.Text = "[plugin name]";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Reports";
+            // 
+            // removeButton
+            // 
+            this.removeButton.Enabled = false;
+            this.removeButton.Location = new System.Drawing.Point(93, 174);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 3;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Enabled = false;
+            this.updateButton.Location = new System.Drawing.Point(174, 174);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.addButton.Enabled = false;
+            this.addButton.Location = new System.Drawing.Point(12, 174);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(502, 276);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 9;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // dateColumnHeader
+            // 
+            this.dateColumnHeader.Text = "Date";
+            // 
+            // textColumnHeader
+            // 
+            this.textColumnHeader.Text = "Text";
+            this.textColumnHeader.Width = -2;
+            // 
+            // ManageReportsForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ManageReportsForm";
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(670, 311);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.searchComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.nextUnhandledButton);
+            this.Controls.Add(this.numUnhandledReportsLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ManageReportsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Reports";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label numUnhandledReportsLabel;
+        private System.Windows.Forms.Button nextUnhandledButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox searchComboBox;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label pluginNameLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox approveCheckBox;
+        private System.Windows.Forms.ListView reportsListView;
+        private System.Windows.Forms.TextBox reportBodyTextBox;
+        private System.Windows.Forms.ColumnHeader dateColumnHeader;
+        private System.Windows.Forms.ColumnHeader textColumnHeader;
     }
 }
