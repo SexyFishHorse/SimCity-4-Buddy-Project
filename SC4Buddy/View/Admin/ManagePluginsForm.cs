bb7bf9a3bb4487ef25f4d0cb7ce1882bde90e9fe
Asbjørn Hansen <asb.han@nihei.dk>
2013-09-26 16:26:37 +0200

    added search logic to search box event actions

--- SC4Buddy/View/Admin/ManagePluginsForm.cs
+++ SC4Buddy/View/Admin/ManagePluginsForm.cs
@@ -1,9 +1,13 @@
 ﻿namespace NIHEI.SC4Buddy.View.Admin
 {
     using System;
+    using System.Linq;
     using System.Windows.Forms;
 
+    using NIHEI.Common.UI.Elements;
     using NIHEI.SC4Buddy.Control.Remote;
+    using NIHEI.SC4Buddy.Entities.Remote;
+
     public partial class ManagePluginsForm : Form
     {
         private readonly RemotePluginController remotePluginController;
@@ -16,6 +20,31 @@
 
         private void SearchComboBoxValueChanged(object sender, EventArgs e)
         {
+            var text = searchComboBox.Text.ToUpper();
+
+            if (text.Length < 3)
+            {
+                return;
+            }
+
+            var matches =
+                remotePluginController.Plugins.Where(
+                    x =>
+                    x.Name.ToUpper().Contains(text) || x.Author.Name.ToUpper().Contains(text)
+                    || x.Description.ToUpper().Contains(text) || x.Link.ToUpper().Contains(text));
+
+            pluginsListView.BeginUpdate();
+            pluginsListView.Items.Clear();
+            
+            foreach (var plugin in matches)
+            {
+                var item = new ListViewItemWithObjectValue<RemotePlugin>(plugin.Name, plugin);
+                item.SubItems.Add(plugin.Author.Name);
+                item.SubItems.Add(plugin.Link);
+            }
+
+            pluginsListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            pluginsListView.EndUpdate();
         }
     }
 }
