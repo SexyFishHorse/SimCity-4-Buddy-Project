652630fdd43231326ccbaba77e7b9db651060d1f
Asbjørn Hansen <asb.han@nihei.dk>
2013-10-02 08:01:17 +0200

    added equality members to remote plugin file

--- SC4Buddy/Entities/Remote/RemotePluginFile.cs
+++ SC4Buddy/Entities/Remote/RemotePluginFile.cs
@@ -2,5 +2,33 @@
 {
     public partial class RemotePluginFile
     {
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj.GetType() == GetType() && Equals((RemotePluginFile)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Name != null ? Name.GetHashCode() : 0) * 397)
+                       ^ (Checksum != null ? Checksum.GetHashCode() : 0);
+            }
+        }
+
+        private bool Equals(RemotePluginFile other)
+        {
+            return string.Equals(Name, other.Name) && string.Equals(Checksum, other.Checksum);
+        }
     }
 }
