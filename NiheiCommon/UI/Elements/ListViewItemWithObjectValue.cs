3d1fe0ac7654b5d54b6d8d8b2f3df9312a287a1e
Asbjørn Hansen <asb.han@nihei.dk>
2013-10-03 11:05:16 +0200

    added equality members to listviewitemwithobjectvalue

--- NiheiCommon/UI/Elements/ListViewItemWithObjectValue.cs
+++ NiheiCommon/UI/Elements/ListViewItemWithObjectValue.cs
@@ -1,5 +1,6 @@
 ﻿namespace NIHEI.Common.UI.Elements
 {
+    using System.Collections.Generic;
     using System.Windows.Forms;
 
     public class ListViewItemWithObjectValue<T> : ListViewItem
@@ -11,5 +12,30 @@
         }
 
         public T Value { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj.GetType() == GetType() && Equals((ListViewItemWithObjectValue<T>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<T>.Default.GetHashCode(Value);
+        }
+
+        protected bool Equals(ListViewItemWithObjectValue<T> other)
+        {
+            return EqualityComparer<T>.Default.Equals(Value, other.Value);
+        }
     }
 }
