8916884dfcb3eb600ce8ae2068a4e9a813b8962e
Asbjørn Hansen <asb.han@nihei.dk>
2013-10-04 07:30:17 +0200

    added updatedependencylistview method

--- SC4Buddy/View/Admin/ManagePlugins/ManagePluginDependenciesForm.cs
+++ SC4Buddy/View/Admin/ManagePlugins/ManagePluginDependenciesForm.cs
@@ -1,8 +1,10 @@
 ﻿namespace NIHEI.SC4Buddy.View.Admin.ManagePlugins
 {
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Windows.Forms;
 
+    using NIHEI.Common.UI.Elements;
     using NIHEI.SC4Buddy.Entities.Remote;
 
     public partial class ManagePluginDependenciesForm : Form
@@ -11,8 +13,26 @@
         {
             Dependencies = pluginDependencies;
             InitializeComponent();
+
+            UpdateDependencyListView(Dependencies);
         }
 
         public Collection<RemotePlugin> Dependencies { get; private set; }
+
+        private void UpdateDependencyListView(IEnumerable<RemotePlugin> dependencies)
+        {
+            dependenciesListView.BeginUpdate();
+            dependenciesListView.Items.Clear();
+
+            foreach (var dependency in dependencies)
+            {
+                var item = new ListViewItemWithObjectValue<RemotePlugin>(dependency.Name, dependency);
+                item.SubItems.Add(dependency.Author.Name);
+
+                dependenciesListView.Items.Add(item);
+            }
+
+            dependenciesListView.EndUpdate();
+        }
     }
 }
