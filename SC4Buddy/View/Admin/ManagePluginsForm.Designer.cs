7419eb8f985ff60b4183a1114093b6693e0a323f
Asbjørn Hansen <asb.han@nihei.dk>
2013-10-15 07:43:03 +0200

    fixed label alignments

--- SC4Buddy/View/Admin/ManagePluginsForm.Designer.cs
+++ SC4Buddy/View/Admin/ManagePluginsForm.Designer.cs
@@ -130,7 +130,7 @@ namespace NIHEI.SC4Buddy.View.Admin
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 75);
+            this.label3.Location = new System.Drawing.Point(6, 75);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(27, 13);
             this.label3.TabIndex = 7;
@@ -170,7 +170,7 @@ namespace NIHEI.SC4Buddy.View.Admin
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 25);
+            this.label1.Location = new System.Drawing.Point(6, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 2;
