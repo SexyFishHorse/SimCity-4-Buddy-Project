cb926c5380cbfec1f21252b54f06527feb95ae87
Asbjørn Hansen <asb.han@nihei.dk>
2014-03-19 08:35:27 +0100

    added properties and equality comparers to plugin group class

--- SC4Buddy/Model/PluginGroup.cs
+++ SC4Buddy/Model/PluginGroup.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NIHEI.SC4Buddy.Model
 {
-    class PluginGroup
+    public class PluginGroup
     {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public PluginGroup(Guid id)
+        {
+            Id = id;
+        }
+
+        protected bool Equals(PluginGroup other)
+        {
+            return Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((PluginGroup)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        private sealed class NameEqualityComparer : IEqualityComparer<PluginGroup>
+        {
+            public bool Equals(PluginGroup x, PluginGroup y)
+            {
+                if (ReferenceEquals(x, y))
+                {
+                    return true;
+                }
+                if (ReferenceEquals(x, null))
+                {
+                    return false;
+                }
+                if (ReferenceEquals(y, null))
+                {
+                    return false;
+                }
+                if (x.GetType() != y.GetType())
+                {
+                    return false;
+                }
+                return string.Equals(x.Name, y.Name);
+            }
+
+            public int GetHashCode(PluginGroup obj)
+            {
+                return (obj.Name != null ? obj.Name.GetHashCode() : 0);
+            }
+        }
+
+        private static readonly IEqualityComparer<PluginGroup> NameComparerInstance = new NameEqualityComparer();
+
+        public static IEqualityComparer<PluginGroup> NameComparer
+        {
+            get
+            {
+                return NameComparerInstance;
+            }
+        }
     }
 }
