0d2327039fb1690392f7c9ae4e6d1f18037c82fa
Asbjørn Hansen <asb.han@nihei.dk>
2014-03-19 08:45:29 +0100

    added properties and equality members to model base class

--- SC4Buddy/Model/ModelBase.cs
+++ SC4Buddy/Model/ModelBase.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NIHEI.SC4Buddy.Model
 {
-    class ModelBase
+    public abstract class ModelBase
     {
+        public Guid Id { get; set; }
+
+        protected ModelBase(Guid id)
+        {
+            Id = id;
+        }
+
+        private bool Equals(ModelBase other)
+        {
+            return Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            return Equals((ModelBase)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
     }
 }
