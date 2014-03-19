2842602deb43bc8c14cf2897bd0f1a386b3217d0
Asbjørn Hansen <asb.han@nihei.dk>
2014-03-19 08:30:04 +0100

    added properties and equality comparer members to userfolder object

--- SC4Buddy/Model/UserFolder.cs
+++ SC4Buddy/Model/UserFolder.cs
@@ -1,12 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NIHEI.SC4Buddy.Model
+﻿namespace NIHEI.SC4Buddy.Model
 {
-    class UserFolder
+    using System;
+    using System.Collections.Generic;
+
+    public class UserFolder
     {
+        public UserFolder(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; set; }
+
+        public string Path { get; set; }
+
+        public string Alias { get; set; }
+
+        private bool Equals(UserFolder other)
+        {
+            return Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((UserFolder)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        private sealed class AliasEqualityComparer : IEqualityComparer<UserFolder>
+        {
+            public bool Equals(UserFolder x, UserFolder y)
+            {
+                if (ReferenceEquals(x, y))
+                {
+                    return true;
+                }
+                if (ReferenceEquals(x, null))
+                {
+                    return false;
+                }
+                if (ReferenceEquals(y, null))
+                {
+                    return false;
+                }
+                if (x.GetType() != y.GetType())
+                {
+                    return false;
+                }
+                return string.Equals(x.Alias, y.Alias);
+            }
+
+            public int GetHashCode(UserFolder obj)
+            {
+                return (obj.Alias != null ? obj.Alias.GetHashCode() : 0);
+            }
+        }
+
+        private static readonly IEqualityComparer<UserFolder> AliasComparerInstance = new AliasEqualityComparer();
+
+        public static IEqualityComparer<UserFolder> AliasComparer
+        {
+            get
+            {
+                return AliasComparerInstance;
+            }
+        }
     }
 }
