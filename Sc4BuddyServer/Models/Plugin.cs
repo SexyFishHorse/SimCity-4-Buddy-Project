019816405f580277256fd69c08482d490ac8a6fd
Asbjørn Hansen <asb.han@nihei.dk>
2014-04-19 00:41:18 +0200

    added properties about plugin to plugin model (not list of files)

--- Sc4BuddyServer/Models/Plugin.cs
+++ Sc4BuddyServer/Models/Plugin.cs
@@ -5,5 +5,19 @@
     public class Plugin
     {
         public Guid Id { get; set; }
+
+        public Version Version { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public string LinkToDownloadPage { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime DateModified { get; set; }
     }
 }
