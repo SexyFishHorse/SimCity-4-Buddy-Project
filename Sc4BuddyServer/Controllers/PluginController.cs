84c4856fa8a65dc84281b929b868e84e1a6c1252
Asbjørn Hansen <asb.han@nihei.dk>
2014-04-19 00:33:53 +0200

    added skeleton methods to the plugin controller

--- Sc4BuddyServer/Controllers/PluginController.cs
+++ Sc4BuddyServer/Controllers/PluginController.cs
@@ -1,8 +1,40 @@
 ﻿namespace Sc4BuddyServer.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
     using System.Web.Http;
+
+    using Sc4BuddyServer.Models;
 
     public class PluginController : ApiController
     {
+        public Plugin GetPlugin(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Plugin> GetAllPlugins()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize]
+        public HttpResponseMessage PostPlugin(Plugin plugin)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize]
+        public void PutPlugin(Plugin plugin)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize]
+        public void DeletePlugin(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
