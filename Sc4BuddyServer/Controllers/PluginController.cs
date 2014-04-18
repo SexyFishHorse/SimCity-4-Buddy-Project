aa25aa5208cc5f0b603abbb8bd72ad3a18328ab9
Asbjørn Hansen <asb.han@nihei.dk>
2014-04-19 00:37:33 +0200

    added repository interface to controller

--- Sc4BuddyServer/Controllers/PluginController.cs
+++ Sc4BuddyServer/Controllers/PluginController.cs
@@ -2,39 +2,64 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Net;
     using System.Net.Http;
     using System.Web.Http;
 
     using Sc4BuddyServer.Models;
+    using Sc4BuddyServer.Services;
 
     public class PluginController : ApiController
     {
+        private readonly IPluginRepository repository;
+
         public Plugin GetPlugin(Guid id)
         {
-            throw new NotImplementedException();
+            return repository.GetPlugin(id);
         }
 
         public IEnumerable<Plugin> GetAllPlugins()
         {
-            throw new NotImplementedException();
+            return repository.GetAllPlugins();
         }
 
         [Authorize]
         public HttpResponseMessage PostPlugin(Plugin plugin)
         {
-            throw new NotImplementedException();
+            try
+            {
+                plugin = repository.AddPlugin(plugin);
+
+                var response = Request.CreateResponse(HttpStatusCode.Created, plugin);
+
+                // ReSharper disable once RedundantAnonymousTypePropertyName
+                var uri = Url.Link("DefaultApi", new { Id = plugin.Id });
+                response.Headers.Location = new Uri(uri);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                                   {
+                                       Content =
+                                           new StringContent(string.Format("Error message: {0}", ex.Message)),
+                                   };
+
+                return response;
+            }
         }
 
         [Authorize]
         public void PutPlugin(Plugin plugin)
         {
-            throw new NotImplementedException();
+            repository.UpdatePlugin(plugin);
         }
 
         [Authorize]
         public void DeletePlugin(Guid id)
         {
-            throw new NotImplementedException();
+            repository.DeletePlugin(id);
         }
     }
 }
---
+++ Sc4BuddyServer/Services/IPluginRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Sc4BuddyServer.Services
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Sc4BuddyServer.Models;
+
+    public interface IPluginRepository
+    {
+        Plugin GetPlugin(Guid id);
+
+        IEnumerable<Plugin> GetAllPlugins();
+
+        Plugin AddPlugin(Plugin plugin);
+
+        void UpdatePlugin(Plugin plugin);
+
+        void DeletePlugin(Guid id);
+    }
+}
