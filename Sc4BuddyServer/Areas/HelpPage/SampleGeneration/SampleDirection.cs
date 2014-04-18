868e44b90753cc5a80be574a6b39d8dae7d7b816
Asbjørn Hansen <asb.han@nihei.dk>
2014-04-18 02:15:58 +0200

    added project and deployed it to server

---
+++ Sc4BuddyServer/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace Sc4BuddyServer.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
