d803342ab8337ddda2b584f1f0ca469fd42bc3b5
Asbjørn Hansen <asb.han@nihei.dk>
2014-05-05 00:22:08 +0200

    added plugin matcher class and implemented ipluginmatcher interface

---
+++ SC4Buddy/Remote/PluginMatcher.cs
@@ -0,0 +1,31 @@
+﻿namespace NIHEI.SC4Buddy.Remote
+{
+    using System.Collections.Generic;
+
+    using Irradiated.Sc4Buddy.ApiClient.Model;
+
+    using PluginFile = NIHEI.SC4Buddy.Model.PluginFile;
+
+    public class PluginMatcher : IPluginMatcher
+    {
+        public IEnumerable<Plugin> GetPossibleRemotePlugins(IList<PluginFile> files)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Plugin GetMostLikelyRemotePlugin(IList<PluginFile> files)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool MatchAndUpdate(Model.Plugin plugin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Plugin GetMostLikelyRemotePluginForFile(string filePath, string checksum)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
