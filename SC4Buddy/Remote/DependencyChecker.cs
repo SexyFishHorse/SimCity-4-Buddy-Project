daac5a3c2442c71a3acb7f6194408a202060ceac
Asbjørn Hansen <asb.han@nihei.dk>
2014-05-13 16:33:00 +0200

    added api client and main user folder as fields and to constructor

--- SC4Buddy/Remote/DependencyChecker.cs
+++ SC4Buddy/Remote/DependencyChecker.cs
@@ -4,12 +4,24 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    using Irradiated.Sc4Buddy.ApiClient;
+
     using NIHEI.SC4Buddy.Model;
 
     using RemotePlugin = Irradiated.Sc4Buddy.ApiClient.Model.Plugin;
 
     public class DependencyChecker : IDependencyChecker
     {
+        private readonly ISc4BuddyApiClient client;
+
+        private readonly UserFolder mainUserFolder;
+
+        public DependencyChecker(ISc4BuddyApiClient client, UserFolder mainUserFolder)
+        {
+            this.client = client;
+            this.mainUserFolder = mainUserFolder;
+        }
+
         public async Task<IEnumerable<RemotePlugin>> CheckDependenciesAsync(UserFolder userFolder)
         {
             throw new NotImplementedException();
