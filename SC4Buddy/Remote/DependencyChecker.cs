68ed1d5cefad450edd00bf67515d1a573fca7ab2
Asbjørn Hansen <asb.han@nihei.dk>
2014-05-13 16:33:34 +0200

    added method to get all known plugins from both main and the current user folder

--- SC4Buddy/Remote/DependencyChecker.cs
+++ SC4Buddy/Remote/DependencyChecker.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using Irradiated.Sc4Buddy.ApiClient;
@@ -25,6 +26,26 @@
         public async Task<IEnumerable<RemotePlugin>> CheckDependenciesAsync(UserFolder userFolder)
         {
             throw new NotImplementedException();
+        }
+
+        private HashSet<Guid> GetAllKnownPluginGuids(UserFolder userFolder)
+        {
+            var knownUserFolderPlugins = userFolder.Plugins.Where(x => x.RemotePlugin != null);
+            var knownMainPlugins = mainUserFolder.Plugins.Where(x => x.RemotePlugin != null);
+
+            var knownPlugins = new HashSet<Guid>();
+
+            foreach (var plugin in knownMainPlugins)
+            {
+                knownPlugins.Add(plugin.RemotePlugin.Id);
+            }
+
+            foreach (var plugin in knownUserFolderPlugins)
+            {
+                knownPlugins.Add(plugin.RemotePlugin.Id);
+            }
+
+            return knownPlugins;
         }
     }
 }
