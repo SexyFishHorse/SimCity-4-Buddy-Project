ebc8cab9ae39578eb8789a21e7d7375e07c6d2d5
Asbjørn Hansen <asser-dk@outlook.com>
2015-01-26 10:13:31 +0100

    implemented missing methods in pluginscontroller

--- SC4Buddy/Plugins/Control/PluginsController.cs
+++ SC4Buddy/Plugins/Control/PluginsController.cs
@@ -1,24 +1,33 @@
 ﻿namespace NIHEI.SC4Buddy.Plugins.Control
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.VisualBasic.FileIO;
     using NIHEI.SC4Buddy.Model;
+    using NIHEI.SC4Buddy.Plugins.DataAccess;
     using NIHEI.SC4Buddy.Remote;
 
     public class PluginsController : IPluginsController
     {
         private readonly IPluginFileController pluginFileController;
 
-        private readonly IPluginController pluginController;
+        private readonly PluginsDataAccess pluginsDataAccess;
 
-        public PluginsController(IPluginFileController pluginFileController, IPluginController pluginController)
+        public PluginsController(IPluginFileController pluginFileController, PluginsDataAccess pluginsDataAccess, UserFolder userFolder)
         {
             this.pluginFileController = pluginFileController;
-            this.pluginController = pluginController;
+            this.pluginsDataAccess = pluginsDataAccess;
+            UserFolder = userFolder;
+
+            Plugins = pluginsDataAccess.LoadPlugins();
         }
+
+        public ICollection<Plugin> Plugins { get; set; }
+
+        public UserFolder UserFolder { get; set; }
 
         public static bool IsDamnFile(UserFolder userFolder, string path)
         {
@@ -45,10 +54,44 @@
             return validFilenames.Any(entity.EndsWith);
         }
 
-        public void UninstallPlugin(Plugin selectedPlugin)
+        public void Add(Plugin plugin)
         {
-            var files = new PluginFile[selectedPlugin.PluginFiles.Count];
-            selectedPlugin.PluginFiles.CopyTo(files, 0);
+            if (Plugins.Any(x => x.Id == plugin.Id))
+            {
+                throw new ArgumentException("A plugin with the id " + plugin.Id + " already exists.");
+            }
+
+            Plugins.Add(plugin);
+            pluginsDataAccess.SavePlugins(Plugins, UserFolder);
+        }
+
+        public void Update(Plugin plugin)
+        {
+            if (Plugins.Any(x => x.Id == plugin.Id))
+            {
+                Plugins.Remove(plugin);
+            }
+
+            Plugins.Add(plugin);
+
+            pluginsDataAccess.SavePlugins(Plugins, UserFolder);
+        }
+
+        public void Remove(Plugin plugin)
+        {
+            if (Plugins.All(x => x.Id != plugin.Id))
+            {
+                return;
+            }
+
+            Plugins.Remove(plugin);
+            pluginsDataAccess.SavePlugins(Plugins, UserFolder);
+        }
+
+        public void UninstallPlugin(Plugin plugin)
+        {
+            var files = new PluginFile[plugin.PluginFiles.Count];
+            plugin.PluginFiles.CopyTo(files, 0);
             foreach (var file in files)
             {
                 if (File.Exists(file.Path))
@@ -59,15 +102,13 @@
                 pluginFileController.Delete(file);
             }
 
-            pluginController.Delete(selectedPlugin);
+            Remove(plugin);
         }
 
         public async Task<int> UpdateInfoForAllPluginsFromServer(IPluginMatcher pluginMatcher)
         {
-            var plugins = pluginController.Plugins;
-
             var count = 0;
-            foreach (var plugin in plugins)
+            foreach (var plugin in Plugins)
             {
                 if (await pluginMatcher.MatchAndUpdateAsync(plugin))
                 {
@@ -80,7 +121,21 @@
 
         public int NumberOfRecognizedPlugins(UserFolder userFolder)
         {
-            return pluginController.Plugins.Count(x => x.RemotePlugin != null && x.UserFolder.Id == userFolder.Id);
+            return Plugins.Count(x => x.RemotePlugin != null);
+        }
+
+        public int RemoveEmptyPlugins()
+        {
+            var emptyPlugins = Plugins.Where(x => !x.PluginFiles.Any()).ToList();
+
+            var counter = emptyPlugins.Count();
+
+            foreach (var emptyPlugin in emptyPlugins)
+            {
+                Remove(emptyPlugin);
+            }
+
+            return counter;
         }
     }
 }
