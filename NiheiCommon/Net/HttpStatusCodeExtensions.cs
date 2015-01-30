ecee4228c3b83294fabba5dc1059f933af1d4298
Asbjørn Hansen <aha@trustpilot.com>
2015-01-30 12:18:23 +0100

    added util for http status codes to check if it is a success status code.

---
+++ NiheiCommon/Net/HttpStatusCodeExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace NIHEI.Common.Net
+{
+    using System.Net;
+
+    public static class HttpStatusCodeExtensions
+    {
+        public static bool IsSuccess(this HttpStatusCode statusCode)
+        {
+            return
+                statusCode == HttpStatusCode.OK ||
+                statusCode == HttpStatusCode.Created ||
+                statusCode == HttpStatusCode.Accepted ||
+                statusCode == HttpStatusCode.NonAuthoritativeInformation ||
+                statusCode == HttpStatusCode.ResetContent ||
+                statusCode == HttpStatusCode.PartialContent;
+        }
+    }
+}
