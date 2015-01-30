0d34e65d3044b20e12d4bcd5ee730a88c6754b3d
Asbjørn Hansen <aha@trustpilot.com>
2015-01-30 12:20:06 +0100

    added util to easily check if the user has a network connection and that the api is configured correctly.

---
+++ SC4Buddy/Remote/Models/BuddyServerException.cs
@@ -0,0 +1,15 @@
+﻿namespace NIHEI.SC4Buddy.Remote.Models
+{
+    using System;
+    using NIHEI.SC4Buddy.Remote.Utils;
+
+    public class BuddyServerException : Exception
+    {
+        public BuddyServerException(string message, ApiConnectCodes code) : base(message)
+        {
+            Code = code;
+        }
+
+        public ApiConnectCodes Code { get; set; }
+    }
+}
---
+++ SC4Buddy/Remote/Utils/ApiConnect.cs
@@ -0,0 +1,40 @@
+﻿namespace NIHEI.SC4Buddy.Remote.Utils
+{
+    using System.Net.NetworkInformation;
+    using NIHEI.SC4Buddy.Configuration;
+    using NIHEI.SC4Buddy.Remote.Models;
+    using RestSharp;
+
+    public class ApiConnect
+    {
+        private readonly string baseUrl;
+
+        public ApiConnect(string baseUrl)
+        {
+            this.baseUrl = baseUrl;
+        }
+
+        public static void ThrowErrorOnConnectionOrDisabledFeature(string feature)
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                throw new BuddyServerException("No internet connection available.", ApiConnectCodes.NoNetworkConnection);
+            }
+
+            if (string.IsNullOrWhiteSpace(Settings.Get(Settings.Keys.ApiBaseUrl)))
+            {
+                throw new BuddyServerException("Api base url is not defined.", ApiConnectCodes.NoBaseApiDefined);
+            }
+
+            if (!Settings.Get<bool>(feature))
+            {
+                throw new BuddyServerException("The feature " + feature + " is disabled.", ApiConnectCodes.FeatureDisabled);
+            }
+        }
+
+        public IRestClient GetClient()
+        {
+            return new RestClient(baseUrl);
+        }
+    }
+}
