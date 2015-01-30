74dfa45116bf6349f9e6abb0a80c293369004800
Asbjørn Hansen <aha@trustpilot.com>
2015-01-30 13:36:39 +0100

    uncommented everything in the dependency checker for later refactoring

--- SC4Buddy/Remote/IDependencyChecker.cs
+++ SC4Buddy/Remote/IDependencyChecker.cs
@@ -2,14 +2,10 @@
 {
     using System.Collections.Generic;
     using System.Threading.Tasks;
-
     using NIHEI.SC4Buddy.Model;
-
-    using RemotePlugin = Irradiated.Sc4Buddy.ApiClient.Model.Plugin;
-    using RemotePluginFile = Irradiated.Sc4Buddy.ApiClient.Model.PluginFile;
 
     public interface IDependencyChecker
     {
-        Task<IEnumerable<RemotePlugin>> CheckDependenciesAsync(UserFolder userFolder);
+        Task<IEnumerable<Plugin>> CheckDependenciesAsync(UserFolder userFolder);
     }
 }
